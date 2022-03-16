63dd2a0d3c0d0e7928d8c54ef0804ae2deac31b3
umairkhan1997 <mohumairkhan47@gmail.com>
2022-03-16 11:40:17 +0500

    class worl

---
+++ Class 18/app.js
@@ -0,0 +1,76 @@
+
+
+// variable//
+
+//  var myname = "Talha" ;
+
+// alert(   myname   )
+
+///
+
+////DATA TYPES////
+// String  "    "
+// Number
+// Boolean
+// undefined
+// null
+
+
+
+// STRING///
+
+// var myName = "Jaffar Aman";
+
+
+///Number//
+// var age = 21;
+
+
+
+// camelCase
+
+// var isMyName  = "jafffar"  ///String
+
+// alert(Name)
+
+
+
+
+
+// var myName = "Talha";
+// var num1 = 20;
+// alert(myName + num1)  
+
+
+// +
+// -
+// *
+// /
+// %
+
+// var num1 = 10;
+// var num2 = 2;
+// alert(num1 % num2)
+
+// var num1 = "20";
+// var  num2 = 30;
+// var add = num1 * num2
+// alert(num1 + num2)
+
+
+
+
+// var num1 = "50";
+// var num2 = 80;
+
+// var add = num1 - num2
+
+// alert(add)
+
+
+// alert("hello")
+// alert("world")
+// alert("class")
+
+
+
---
+++ class 19/app.js
@@ -0,0 +1,106 @@
+// alert("HELLO CLASS")
+
+// variables
+
+// var myName = "JAFFAR AMAN"; ///STRING
+
+// alert(myName);
+// console.log(myName)
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+///STRING
+///NUMBER
+
+// var myName = "jaffar"; ///string
+
+// var age = "21"; ///number
+
+// console.log(myName);
+// console.log(age);
+
+// var add = myName + age;
+
+// var num1 = "20";
+// var num2 = 50;
+
+// var firstName = "jaffar"
+// var lastName = "aman"
+// var fullName = firstName + " " +   lastName
+
+// var result = num1 + num2;
+
+// console.log(result);
+// console.log(fullName)
+
+// var result = 10 % 3;
+
+// console.log(result)
+
+// var myName = "Jaffar";
+// console.log(myName);
+
+// myName = "JAFFAR AMAN";
+// console.log(myName);
+
+// var myname;
+// console.log(myname);
+
+// myname = "JAFFAR AMAN"
+// console.log(myname);
+
+// var num = 10; //+ 1 = 11
+// num++;
+
+// console.log(num++);//10  ///post
+// console.log(num);
+
+// console.log(++num); ///pre
+// console.log(num);
+
+// console.log(num--); ///post
+// console.log(num);
+
+// console.log(--num);  ///pre
+
+// var result = num++ + num + num++;
+//  15    + 16  + 16
+
+// var result = num + ++num + num++ + ++num
+//  15  + 16    + 16    +  18
+
+// var num = 15; // 16  /// 17
+// var result = num++ + num++ + num + ++num + num;
+//   15   +  16   + 17  +  18   + 18
+
+// var num = 18; // 21  // 22  / 23
+// var result = num - num++ + ++num - ++num + num;
+//  20  -  20   +  22   -  23   + 23
+// var result = num++ - ++num + ++num + num + ++num
+
+// var num = 14; // 13  / 12
+// var result = num-- + ++num + num + num++ - --num;
+// var result = num-- - --num + ++num + ++num + num - num++ - num--;
+//  14    -  12   + 13    +  14   + 14  -  14   - 15
+//     2     + 13     +14 - 15
+//     + 14
+
+// console.log(result);
+
+// var num = 10; //11
+
+// num++;  //10
+
+// console.log(num);
+
+// var result = 50 + 20 + (10 + 20 - 10 * 2);
+// var result = 1 + 3 * 4; ///4 * 4
+// console.log(result);
+
+// var name = "jaffar"
+
+////USER INPUT
+// var name = prompt("NAME" , "talha" );
+
+// console.log(name);
