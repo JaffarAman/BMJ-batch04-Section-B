608e9da8b42d4586d39cf734b492f3d340d076a6
umairkhan1997 <mohumairkhan47@gmail.com>
2022-04-02 11:50:52 +0500

    array & loop

---
+++ class 22/app.js
@@ -0,0 +1,151 @@
+// console.log("Hello class");
+
+// var arrName = ["Jaffar", 21, true, undefined, null];
+
+// console.log(arrName.length   )
+
+// console.log(arrName[8]);
+
+// arrName = [];
+// arrName[0] = "yousuf";
+// arrName[5] = "talha";
+
+// console.log(arrName);
+
+// var arrName = ["Jaffar", 21, true, undefined, null];
+
+//remove last value
+// arrName.pop();
+
+// value add in last
+// arrName.push("abc", "xyz");
+
+// arrName.shift()
+// arrName.unshift("abcd" , "xyz")
+
+///slice
+
+// var arrName = ["karachi", "lahore", "multan", "islamabad"];
+
+// var arrCopy = arrName.slice(1, 3);
+
+// var arrCopy = arrName.slice(0, -2);
+// var arrCopy = arrName.slice(-4);
+
+// console.log(arrCopy);
+
+// var arrName = ["karachi", "lahore", "multan", "islamabad"];
+
+// arrName.splice(1, 0, "jaffar");
+// console.log(arrName);
+
+////for loop///
+
+// for(var i = 1 ; i <=10 ; i++ ){
+//     console.log(i)
+// }
+
+// if (false) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// for (var i = 1; i < 5; i++) {
+//   document.write(i + "<br />");
+// }
+
+// document.write(1 + "<br />");
+// document.write(2 + "<br />");
+// document.write(3 + "<br />");
+// document.write(4 + "<br />");
+
+// document.write("2 x 1 = " + 2 * 1 + "<br />");
+// document.write("2 x 2 = " + 2 * 2 + "<br />");
+// document.write("2 x 3 = " + 2 * 3 + "<br />");
+
+// for (var i = 1; 101 <= 100; i++) {
+//   document.write("2 x " + i + " = " + 2 * i + "<br />");
+// }
+
+// var tableNumber = +prompt("enter table number");
+// var tablelength = +prompt("enter table length");
+
+// if (tableNumber == false) {
+//   tableNumber = 5;
+// }
+// if (!tablelength) {
+//   tablelength = 10;
+// }
+
+// if(startingPoint > end){
+//     alert()
+// }else{
+
+// }
+
+// for (var i = 1; i <= tablelength; i++) {
+//   document.write(tableNumber + " x " + i + " = " + tableNumber * i + "<br />");
+// }
+
+
+// console.log(cities[0]);
+// console.log(cities[1]);
+// console.log(cities[2]);
+// console.log(cities[3]);
+
+// var userCity = prompt("enter city name");
+
+// if(userCity == cities[0]){
+//     alert("welcome")
+// }else if (userCity == cities[1]){
+//     alert("welcome")
+// }
+// else if (userCity == cities[2]){
+//     alert("welcome")
+// }
+// else if (userCity == cities[3]){
+//     alert("welcome")
+// }
+// else if (userCity == cities[4]){
+//     alert("welcome")
+// }
+// var cities = ["lahore", "multan", "karachi", "islamabad"];
+
+// var userCity = prompt("Enter Your City Name");
+
+// for (var i = 0; i < cities.length; i++) {
+//   console.log(cities[i]);
+//   if (userCity == cities[i]) {
+//     alert("City Matches");
+//     break;
+//   }
+// }
+
+
+// for (var i = 0; i < 4; i++) {
+//   console.log(cities[i]);
+// }
+
+
+
+
+
+
+// it gives the ASCII code of any particular character
+// .charCodeAt()
+
+// var input = prompt("Enter any character")
+// if(input.charCodeAt() >= 65 && input.charCodeAt() <= 90) {
+//     alert("It is an UpperCase Character")
+// }
+// else if(input.charCodeAt() >= 97 && input.charCodeAt() <= 122) {
+//     alert("It is a LowerCase Character")
+// }else {
+//     alert("It is a number")
+// }
+
+
+
+
+
---
+++ class 23/app.js
@@ -0,0 +1,147 @@
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for(var i = 10 ; i >= 1 ; i--){
+//     console.log(i)
+// }
+
+// var i = "1";  //number
+// // i = i + 5;
+
+// i += 5;
+
+// console.log(i);
+
+//odd
+// for (var i = 1; i <= 10; i += 2) {
+//     console.log(i)
+// }
+
+// for (var i = 2; i <= 10; i += 2) {
+//     console.log(i)
+// }
+
+// console.log(cities.length);
+// console.log(cities[0])
+// console.log(cities[1])
+// console.log(cities[2])
+// console.log(cities[3])
+
+// for (var i = 0; i < cities.length; i++) {
+//   console.log(cities[i]);
+// }
+
+// var userCity = prompt("enter city");
+
+// if(userCity === cities[0]){
+//     console.log('welcome')
+// }else if(userCity === cities[1]){
+//     console.log('welcome')
+
+// }
+// else if(userCity === cities[2]){
+//     console.log('welcome')
+
+// }
+
+// var cities = ["karachi", "lahore", "multan", "islamabad"];
+// var isCityMatch = false;
+// for (var i = 0; i < cities.length; i++) {
+//   if (userCity === cities[i]) {
+//     isCityMatch = true;
+//     console.log("welcome");
+//     break;
+//   }
+// }
+
+// if (isCityMatch === false) {
+//   console.log("not found");
+// }
+
+// for (var i = 1; i <= 5; i++) {
+//   console.log("jaffar", "outer loop");
+
+//   for (var j = 1; j <= 5; j++) {
+//     console.log("aman", "inner loop");
+//   }
+// }
+
+// var arr = [
+//   [1, 2],
+//   [3, 4, 121, 45, 7456, 45],
+//   [5, 6, 7],
+// ];
+
+// console.log(arr);
+
+// for (var row = 0; row < arr.length; row++) {
+//   for (var col = 0; col < arr[row].length; col++) {
+//     console.log(arr[row][col]);
+
+//   }
+// }
+
+///object
+// var arr = new Array(10)
+// arr[0]
+
+// var arr = [];
+// document.write(i + 1 + ")")
+
+// var nameArr= [asd,afs,af]
+// var nameArr= [30,5,4,]
+// per1 = scoreArr[0] / 500 * 100
+// per1
+// per1
+
+// Initialize an array with color names. Display the array
+// elements in your browser.
+// a. Ask the user what color he/she wants to add to the
+// beginning & add that color to the beginning of the array.
+// Display the updated array in your browser.
+// b. Ask the user what color he/she wants to add to the end
+// & add that color to the end of the array. Display the
+// updated array in your browser.
+// c. Add two more color to the beginning of the array.
+// Display the updated array in your browser.
+// d. Delete the first color in the array. Display the updated
+// array in your browser.
+// e. Delete the last color in the array. Display the updated
+// array in your browser.
+// f. Ask the user at which index he/she wants to add a color
+// & color name. Then add the color to desired
+// position/index. . Display the updated array in your
+// browser.
+// g. Ask the user at which index he/she wants to delete
+// color(s) & how many colors he/she wants to delete. Then
+// Arrays | JAVASCRIPT
+// Page 3 of 6
+// remove the same number of color(s) from user-defined
+// position/index. . Display the updated array in your
+// browser.
+
+// var colorName = [];
+// var firstValue = prompt("enter color nameto the beginning ");
+// colorName.unshift(firstValue);
+// console.log(colorName);
+// var secondValue = prompt("enter color nameto the end ");
+// colorName.push(secondValue);
+// console.log(colorName);
+
+// var val1 = prompt("enter color first ");
+// var val2 = prompt("enter color second ");
+// colorName.unshift(val1, val2);
+
+// console.log(colorName);
+
+// colorName.shift()
+// console.log(colorName);
+
+var arr = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haie"];
+
+document.write("<select>");
+document.write("<option>" + arr[0] + "</option>");
+document.write("<option>");
+
+document.write("</select>");
